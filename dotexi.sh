a264ac47fef36a0b1310f7ddc46e7d0130a0bee9
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-21 12:13:16 +0200

    dotexi.sh: add copyright note

--- dotexi.sh
+++ dotexi.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
-###############################################################################
-# NAME: dotexi.sh
-# DESCRIPTION: Script to make the final PDF file from .po file
-# NOTES: Only work for this case
-###############################################################################
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 po4a-translate -f texinfo -m orgguide.texi -p orgguide.es.po -l orgguide.es.texi
 LIN_INI=`grep -n '@settitle' orgguide.es.texi | cut -d: -f1`
@@ -20,4 +34,3 @@ cat orgguide.es.tmp2 >> orgguide.es.texi
 cat orgguide.es.tmp3 >> orgguide.es.texi
 cat orgguide.es.tmp4 >> orgguide.es.texi
 cat orgguide.es.tmp5 >> orgguide.es.texi
-
